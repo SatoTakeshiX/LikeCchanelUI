02c882e7f606cf36a1464356307b77ebec26a0ea
satoutakeshi <sato.takeshi1987@gmail.com>
2015-09-07 00:41:00 +0900

    初回コミット

--- LikeCchanelUI/ViewController.m
+++ LikeCchanelUI/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIView *topView;
+@property (weak, nonatomic) IBOutlet UIView *bottomView;
+@property (weak, nonatomic) IBOutlet UIScrollView *myscroll;
 
 @end
 
@@ -17,11 +20,58 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.myscroll.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)animationTransform:(UIView *)view duration:(float)duration delay:(float)delay {
+    CABasicAnimation* animation;
+    animation = [CABasicAnimation animationWithKeyPath:@"transform"];
+    animation.duration = duration;
+    //animation.beginTime = delay;
+    //animation.autoreverses = YES;
+    animation.repeatCount = 0;
+    
+    CATransform3D   transform;
+    transform = CATransform3DIdentity;
+    animation.fromValue = [NSValue valueWithCATransform3D:transform];
+    
+    transform = CATransform3DMakeRotation(M_PI, 1.0f, 0.0f, 0.0f);
+    animation.toValue = [NSValue valueWithCATransform3D:transform];
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    
+    [view.layer addAnimation:animation forKey:@"transformAnimation"];
+}
+
+#pragma mark - ScrollDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+
+
+    CATransform3D transform = CATransform3DIdentity;
+    
+    float rotate = self.myscroll.contentOffset.y / 5;
+    
+    NSLog(@"rotateの値:%f",rotate);
+    
+    transform = CATransform3DRotate(transform, rotate * M_PI / 180.0f, 1.0f, 0.0f, 0.0f);
+    
+    transform.m34 = 1.0f / -300.0f;
+    
+    rotate = rotate * 10;
+    NSLog(@"10倍rotateの値:%f",rotate);
+    transform = CATransform3DTranslate(transform, 1.0f, -rotate, 1000.0f);
+
+    // topView に逆行列を適用する
+    CATransform3D invertTransform = CATransform3DInvert(transform);
+    
+    self.topView.layer.transform = invertTransform;
+    
 }
 
 @end
